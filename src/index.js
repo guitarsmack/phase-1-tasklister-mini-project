536a55ac408b0f1eba23f75a0b40168fe07614e8
Michael Alan Stowe <mastowe94@gmail.com>
2023-06-26 00:43:57 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    buildToDo(e.target["new-task-description"].value)
+    form.reset()
+  })
 });
+
+function buildToDo(thing){
+  let li = document.createElement("li")
+  li.textContent = thing + " "
+  let btn = document.createElement("button")
+  btn.addEventListener("click", handleDelete)
+  btn.textContent = "X"
+  li.appendChild(btn)
+  document.querySelector("ul").appendChild(li)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
