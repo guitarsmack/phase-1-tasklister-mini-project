fa8c9aba80f28e9fce5754b2b1dbdffaa94a4861
Michael Alan Stowe <mastowe94@gmail.com>
2023-06-26 15:52:40 -0500

    some extra

--- src/index.js
+++ src/index.js
@@ -10,11 +10,31 @@ document.addEventListener("DOMContentLoaded", () => {
 function buildToDo(thing){
   let li = document.createElement("li")
   li.textContent = thing + " "
+  let taskPriority = document.querySelector("select").selectedIndex
+  li.style.color = importance(taskPriority)
   let btn = document.createElement("button")
   btn.addEventListener("click", handleDelete)
   btn.textContent = "X"
   li.appendChild(btn)
   document.querySelector("ul").appendChild(li)
+}
+
+function importance(index){
+  switch(index){
+    case 0:
+      color = "black"
+      break;
+    case 1:
+      color = "red"
+      break;
+    case 2:
+      color = "orange"
+      break;
+    case 3:
+      color = "green"
+  }
+
+  return color
 }
 
 function handleDelete(e){
